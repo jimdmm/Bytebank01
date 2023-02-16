e6956e5a2352911fc9015629c9ec46520e86740c
Jim Douglas M. M. Ferreira <ferreira.jim@SERVICES.LOCAL>
2023-02-16 16:38:24 -0300

    Class constructor

---
+++ Bytebank/Program.cs
@@ -0,0 +1,19 @@
+﻿using Bytebank.Contas;
+using Bytebank.Titular;
+using System;
+
+//ContaCorrente conta = new ContaCorrente();
+//conta3.Numero_agencia = 18;
+//conta3.Conta = "1010-H";
+//conta3.SetSaldo(500);
+//Console.WriteLine("Seu saldo é: R$ " +conta3.Getsaldo());
+//Console.WriteLine("Nº de Agência: " +conta3.Numero_agencia);
+//Console.WriteLine("Conta: " +conta3.Conta);
+
+
+ContaCorrente conta2 = new ContaCorrente(18, "1010-X");
+conta2.SetSaldo(500);
+conta2.Titular = new Cliente();
+
+Console.WriteLine(conta2.Getsaldo());
+Console.WriteLine(conta2.Numero_agencia);
---
+++ Bytebank/Titular/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.Titular
+{
+    public class Cliente {
+    
+        public string NomeCompleto { get; set; }
+        public string Cpf { get; set; }
+        public string Profissao { get; set; }
+
+    }
+}
